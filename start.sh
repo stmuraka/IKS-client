691a2e1a38ff4b9f756a6339a8ce7064890909c3
Shaun Murakami <stmuraka@us.ibm.com>
2021-05-12 14:22:17 -1000

    Update default env; Add oc login if targeting ROKS - set default Helm version to v3 - set default Region to us-south - skips calico config for ROKS cluster - add oc login to startup

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-HELM=${HELM:-2}
+HELM=${HELM:-3}
+REGION=${REGION:-us-south}
 
 # Display options
 echo "Environment variable options:
@@ -10,19 +11,19 @@ echo "Environment variable options:
     API_ENDPOINT (default: cloud.ibm.com)                 = ${API_ENDPOINT}
     ACCOUNT_ID (long id)                                  = ${ACCOUNT_ID}
     CLUSTER (IKS cluster name)                            = ${CLUSTER}
-    HELM (Helm version name)                              = ${HELM}
-    REGION (e.g. us-south)                                = ${REGION}
+    HELM (Helm version)                                   = ${HELM}
+    REGION (target region)                                = ${REGION}
     RESOURCE_GROUP (default: default)                     = ${RESOURCE_GROUP}
     USERNAME (IAM username)                               = ${USERNAME}
     SSO (to enable, set =1)                               = ${SSO}
     TEST (set the API Endpoint to test.cloud.ibm.com)     = ${TEST}
 "
 
-# If HELM=3 is specified, change the symlink
-if [[ ${HELM} -eq 3 ]]; then
+# If HELM=2 is specified, change the symlink
+if [[ ${HELM} -eq 2 ]]; then
     rm /usr/local/bin/helm
-    helm3_path=$(ls /usr/local/bin/helm-v3* 2>/dev/null) || { echo "WARNING: Failed to get helm 3 path"; }
-    ln -s ${helm3_path} /usr/local/bin/helm
+    helm2_path=$(ls /usr/local/bin/helm-v2* 2>/dev/null) || { echo "WARNING: Failed to get helm 2 path"; }
+    ln -s ${helm2_path} /usr/local/bin/helm
 fi
 
 # Update the cli if available
@@ -34,6 +35,9 @@ ibmcloud plugin update --all --force
 #for p in ${ic_plugins[*]}; do
 #    ibmcloud plugin update ${p} -f
 #done
+
+# Initialize the oc plugin
+ibmcloud oc init
 
 echo ""
 
@@ -107,17 +111,41 @@ fi
 
 # Setup cluster config
 ibmcloud ks cluster config --cluster ${CLUSTER} --network >/dev/null 2>&1
-kubectx=$(kubectl config view -o jsonpath='{.current-context}')
-cluster_name=$(echo "${kubectx}" | sed -e 's/\/admin//')
-KUBECONFIG=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"${cluster_name}\")].cluster.certificate-authority}")
-CERTS_DIR=$(dirname ${KUBECONFIG})
 
-# Copy Calico config
-mkdir -p /etc/calico
-cp ${CERTS_DIR}/calicoctl.cfg /etc/calico/calicoctl.cfg
+# Setup calico config if cluster is an iks classic cluster
 
-# Disable usage collection
-# No longer valid in v 1.3+
-# ibmcloud config --usage-stats-collect false 2>/dev/null
+# Get classic cluster
+classic_clusters=()
+mapfile -t classic_clusters< <(ibmcloud ks clusters --provider classic --output json | jq -r '.[].name')
+declare -A "clusters=( $(echo ${classic_clusters[@]} | sed 's/[^ ]*/[&]="classic"/g') )"
+
+if [[ "${clusters[${CLUSTER}]}" == "classic" ]]; then
+    echo -n "Setting up calicoctl..."
+    kubectx=$(kubectl config view -o jsonpath='{.current-context}')
+    cluster_name=$(echo "${kubectx}" | sed -e 's/\/admin//')
+    KUBECONFIG=$(kubectl config view -o jsonpath="{.clusters[?(@.name==\"${cluster_name}\")].cluster.certificate-authority}")
+    CERTS_DIR=$(dirname ${KUBECONFIG})
+
+    # Copy Calico config
+    mkdir -p /etc/calico
+    cp ${CERTS_DIR}/calicoctl.cfg /etc/calico/calicoctl.cfg
+    echo "done"
+else # assume vpc-gen2 cluster
+    # get cluster info
+    cluster_info=$(ibmcloud oc cluster get --cluster ${CLUSTER} --output json) || { echo "FAILED to get cluster [${CLUSTER}] info"; exit 1; }
+    [[ -z ${cluster_info} ]] && { echo "FAILED to get cluster [${CLUSTER}] info"; exit 1; }
+    # Setup OC cli
+    echo "Get an OTP from: https://iam.cloud.ibm.com/identity/passcode"
+    read -s -p 'IAM OTP: ' IAM_OTP
+    echo ""
+    # get the master url
+    master_url=$(echo "${cluster_info}" | jq -r '.masterURL')
+    [[ -z ${master_url} ]] && { echo "FAILED to get the master URL for the cluster [${CLUSTER}]"; exit 1; }
+
+    # login to the OpenShift Cluster
+    echo -n "Logging into OpenShift cluster ${CLUSTER}..."
+    /lib/ld-musl-x86_64.so.1 --library-path /lib /usr/local/bin/oc login -u passcode -p ${IAM_OTP} --server=${master_url}
+fi
+
 
 bash
