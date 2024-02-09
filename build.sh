511fbebda6de4694ffade43c5a0930f8dc1acf2b
Shaun Murakami <stmuraka@us.ibm.com>
2024-02-09 09:46:45 -1000

    fix image pull

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+set -eo pipefail
+
 # Build the iks-client image
 
 IMAGE_NAME="iks-client"
-docker pull apline:latest
+docker pull alpine:latest
 docker build -t ${IMAGE_NAME} .
