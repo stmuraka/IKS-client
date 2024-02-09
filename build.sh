c06cb22bc4e7dfde8f19a60cbee3793655e8f706
Shaun Murakami <stmuraka@us.ibm.com>
2024-02-09 10:11:45 -1000

    add platform type to build script

--- build.sh
+++ build.sh
@@ -5,5 +5,7 @@ set -eo pipefail
 # Build the iks-client image
 
 IMAGE_NAME="iks-client"
+PLATFORM="linux/arm64" # Macbook Pro M1
+
 docker pull alpine:latest
-docker build -t ${IMAGE_NAME} .
+docker buildx build --platform ${PLATFORM} -t ${IMAGE_NAME} .
